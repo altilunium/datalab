586002fe298522a3b551fec5aba5dab86e9aa2e5
Altilunium <70379302+altilunium@users.noreply.github.com>
2021-08-27 06:37:32 +0700

    Add files via upload

---
+++ 32bit/bits.h
@@ -0,0 +1,52 @@
+/* Copyright (C) 1991-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+int i_dont_need_highheels(int);
+int test_i_dont_need_highheels(int);
+int BIGGER_BETTER(int, int);
+int test_BIGGER_BETTER(int, int);
+int urgh(int, int);
+int test_urgh(int, int);
+int ocd(int);
+int test_ocd(int);
+int poof(int, int, int);
+int test_poof(int, int, int);
+int limit_break(int, int);
+int test_limit_break(int, int);
+int kitkat_storm(int);
+int test_kitkat_storm(int);
+int float_fishy_storm(unsigned);
+int test_float_fishy_storm(unsigned);
+unsigned float_bad(unsigned);
+unsigned test_float_bad(unsigned);
+unsigned float_sixpack(unsigned);
+unsigned test_float_sixpack(unsigned);
